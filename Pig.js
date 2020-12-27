8cc7acbaeb5aaa8a1fa86e9008cd91f711317275
lasya2007 <75254763+lasya2007@users.noreply.github.com>
2020-12-27 17:38:08 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,10 @@
+class Pig extends baseClass {
+    constructor(x, y) {
+      super(x,y,50,50);
+      this.image=loadImage("sprites/enemy.png");
+    }
+    display(){
+      super.display();
+    }
+  };
+  
